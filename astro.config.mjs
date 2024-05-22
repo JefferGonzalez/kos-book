2b7bbb31905dc4ed43aed60cd8289340474571b7
JefferGonzalez <jefferd2016@gmail.com>
2024-05-21 23:42:26 -0500

    Test Markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import tailwind from '@astrojs/tailwind';
-
-import auth from "auth-astro";
+import { defineConfig } from 'astro/config'
+import auth from 'auth-astro'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind(), auth(), react()],
   output: 'server',
   adapter: vercel()
-});
+})
