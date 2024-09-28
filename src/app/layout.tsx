5871c9838ae6098a570f565b9eb8406e04d66c0f
samuel4x <samuel44acosta@gmail.com>
2024-09-28 13:48:17 -0500

    add session provider encapsular app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import '@/styles/globals.css'
 import NavBar from '@/components/common/NavBar'
 import { ThemeProvider } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
 
 export const metadata: Metadata = {
   title: "KOS'BOOK | The next Documentation Tool",
@@ -28,7 +29,9 @@ export default function RootLayout({
             <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]'></div>
           </div>
         </div>
-
+        <SessionProvider >
+          
+        
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
@@ -40,6 +43,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   )
