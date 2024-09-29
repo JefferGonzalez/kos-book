f8597fe28023458bae6f3d98b3d4929d2d8bd860
JefferGonzalez <jefferd2016@gmail.com>
2024-09-29 11:29:22 -0500

    feat: Update authentication config

--- src/auth.ts
+++ src/auth.ts
@@ -1,25 +1,17 @@
-import NextAuth from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '@/server/db'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth from 'next-auth'
 import Github from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
-import {
-  GITHUB_CLIENT_SECRET,
-  GITHUB_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_ID
-} from '@/config'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     Google({
-      clientId: GOOGLE_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET
+      allowDangerousEmailAccountLinking: true
     }),
     Github({
-      clientId: GITHUB_ID,
-      clientSecret: GITHUB_CLIENT_SECRET
+      allowDangerousEmailAccountLinking: true
     })
   ]
 })
