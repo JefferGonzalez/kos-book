5a94695a6015f4f71af27ad3d05fda3db1730c93
JefferGonzalez <jefferd2016@gmail.com>
2024-10-06 12:40:50 -0500

    feat: Update Toaster component to use rich colors

--- src/components/common/Toaster.tsx
+++ src/components/common/Toaster.tsx
@@ -6,5 +6,5 @@ import { Toaster as SonnerToaster } from 'sonner'
 export default function Toaster() {
   const { theme } = useTheme()
 
-  return <SonnerToaster theme={theme === 'dark' ? 'dark' : 'light'} />
+  return <SonnerToaster theme={theme === 'dark' ? 'dark' : 'light'} richColors />
 }
