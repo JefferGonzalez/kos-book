53b73ba4494931634fa8fcf7cb7ad2a174b23e34
JefferGonzalez <jefferd2016@gmail.com>
2024-10-11 11:35:40 -0500

    fix: Unused variables

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,84 +1,26 @@
 'use client'
 
-import React, { useState } from 'react'
-import { SearchBar } from '@/components/dashboard/search'
 import { ProjectCard } from '@/components/dashboard/card'
+import { SearchBar } from '@/components/dashboard/search'
+import { Project } from '@prisma/client'
 import { useRouter } from 'next/navigation'
-import "@/styles/dashboard.css"
+import { useState } from 'react'
 
+import '@/styles/dashboard.css'
+interface Props {
+  projects: Project[]
+}
 
-const mockProjects = [
-  {
-    id: '1',
-    name: 'Project Alpha',
-    description: 'This is a description for Project Alpha.',
-  },
-  {
-    id: '2',
-    name: 'Project Beta',
-    description: 'This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.',
-  },
-  {
-    id: '3',
-    name: 'Project Gamma',
-    description: 'This is a description for Project Gamma.',
-  },
-  {
-    id: '1',
-    name: 'Project Alpha',
-    description: 'This is a description for Project Alpha.',
-  },
-  {
-    id: '2',
-    name: 'Project Beta',
-    description: 'This is a description for Project Beta. This is a description for Project Beta.This is a description for Project Beta.',
-  },
-  {
-    id: '3',
-    name: 'Project Gamma',
-    description: 'This is a description for Project Gamma.This is a description for Project Beta.',
-  },
-  {
-    id: '1',
-    name: 'Project Alpha',
-    description: 'This is a description for Project Alpha.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.',
-  },
-  {
-    id: '2',
-    name: 'Project Beta',
-    description: 'This is a description for Project Beta. This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.This is a description for Project Beta.',
-  },
-  {
-    id: '3',
-    name: 'Project Gamma',
-    description: 'This is a description for Project Gamma.This is a description for Project Beta.',
-  },
-  {
-    id: '1',
-    name: 'Project Alpha',
-    description: 'This is a description for Project Alpha.',
-  },
-  {
-    id: '2',
-    name: 'Project Beta',
-    description: 'This is a description for Project Beta.This is a description for Project Beta.',
-  },
-  {
-    id: '3',
-    name: 'Project Gamma',
-    description: 'This is a description for Project Gamma.',
-  },
-]
 const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+  const letters = '0123456789ABCDEF'
+  let color = '#'
   for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)]
   }
-  return color;
-};
+  return color
+}
 
-export default function Dashboard({projects}: Props) {
+export default function Dashboard({ projects }: Props) {
   const router = useRouter()
   const [searchTerm, setSearchTerm] = useState('')
 
@@ -89,39 +31,35 @@ export default function Dashboard({projects}: Props) {
   const handleEditProject = (projectId: string) => {
     router.push(`/dashboard/preview/${projectId}`)
   }
+
   const filteredProjects = projects.filter((project) =>
     project.name.toLowerCase().includes(searchTerm.toLowerCase())
   )
-  
 
   return (
-    <div className="p-6 max-w-7xl mx-auto">
-    <h1 className="text-3xl font-extrabold mb-4">Project Dashboard</h1>
-    <SearchBar
-      placeholder="Search projects..."
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-    />
-    {filteredProjects.length > 0 ? (
-      <div className="masonry-grid mt-6">
-        {filteredProjects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.name}
-            description={project.description}
-            onEdit={() => handleEditProject(project.id)}
-            onVisit={handleVisitProject}
-            backgroundColor={getRandomColor()} 
-          
-      
-          />
-        ))}
-      </div>
-    ) : (
-      <div className="text-center mt-3 text-gray-500">No projects found.</div>
-    )}
-  </div>
-)
+    <div className='p-6 max-w-7xl mx-auto'>
+      <h1 className='text-3xl font-extrabold mb-4'>Project Dashboard</h1>
+      <SearchBar
+        placeholder='Search projects...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredProjects.length > 0 ? (
+        <div className='masonry-grid mt-6'>
+          {filteredProjects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              title={project.name}
+              description={project.description}
+              onEdit={() => handleEditProject(project.id)}
+              onVisit={handleVisitProject}
+              backgroundColor={getRandomColor()}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className='text-center mt-3 text-gray-500'>No projects found.</div>
+      )}
+    </div>
+  )
 }
-
-
