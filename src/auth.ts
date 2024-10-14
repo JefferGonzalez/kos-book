379fcef3b65c952e68aca52ff05d9480058479d8
JefferGonzalez <jefferd2016@gmail.com>
2024-10-14 15:33:57 -0500

    feat: Add access token

--- src/auth.ts
+++ src/auth.ts
@@ -10,10 +10,19 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   },
   pages: { signIn: '/auth' },
   callbacks: {
+    jwt: async ({ token, account }) => {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+
+      return token
+    },
     session: async ({ token, session }) => {
       if (token.sub && session.user) {
         session.user.id = token.sub
       }
+
+      session.accessToken = token.accessToken as string
 
       return session
     }
